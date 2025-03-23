6ed2c450d6b15d9c471bc7566db360a24229f508
epiglottis <epiglottis.cartialge@gmail.com>
2025-03-23 23:40:17 +0800

    libr

---
+++ libr/src/lib.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![feature(linkage)]
+#![feature(alloc_error_handler)]
+
+#[macro_use]
+pub mod console;
+mod lang_items;
+mod syscall;
+
+use syscall::*;
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.entry")]
+pub extern "C" fn _start() -> ! {
+    exit(main());
+}
+
+#[linkage = "weak"]
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+pub fn read(fd: usize, buf: &mut [u8]) -> isize {
+    sys_read(fd, buf)
+}
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+pub fn exit(exit_code: i32) -> ! {
+    sys_exit(exit_code);
+}
