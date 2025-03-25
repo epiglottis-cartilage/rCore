803e2bdd16b8878d5756512e1f0ca48fa4cdc12c
epiglottis <epiglottis.cartialge@gmail.com>
2025-03-25 23:56:06 +0800

    user apps

---
+++ user/sleep/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate libr;
+
+use libr::{get_time, r#yield};
+
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    let current_timer = get_time();
+    let wait_for = current_timer + 3000;
+    while get_time() < wait_for {
+        yield_();
+    }
+    println!("Test sleep OK!");
+    0
+}
